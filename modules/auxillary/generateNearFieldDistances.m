3b4dec26167eba6e74c62113f9bb630c67abbbbf
Jacob Rezac <jakealator@gmail.com>
2017-02-01 14:04:55 -0500

    About to add near field matrix assembly. Adding commit point before everything breaks.

--- modules/auxillary/generateNearFieldDistances.m
+++ modules/auxillary/generateNearFieldDistances.m
@@ -2,23 +2,33 @@
 % Created: 02-01-2017 by JDR in Newark
 % Last Modified: 
 %
-% 
+% Input: centroids       - Nx1 matrix containing centroids of each element
+%        multipoleMatrix - NxN sparse matrix such that (i,j)=1 if element i
+%                          and j are within 1 element of each other (are
+%                          touching)
+%        N               - Number of elements
+%        M               - Multipole expansion (needs to be 1 or 0 right now)
+% Output: nearFieldDistances: NxN sparse matrix containing the distances
+%         between elements within M elements of each other. 
 
-function nearFieldDistances = generateNearFieldDistances(centroids, multipoleMatrices, N,M)
+function nearFieldDistances = generateNearFieldDistances(centroids, multipoleMatrix, N,M)
 
-if M>1
+if M==0 % when M=0, treat everything as 'near field.' 
+    nearFieldDistances = sqrt(bsxfun(@plus,full(dot(centroids',centroids',1)),full(dot(centroids',centroids',1))')-full(2*(centroids*centroids')));
+elseif M==1
+    % Find triangles that are in the near field of each other
+    [I,J] = ind2sub([N,N],find(multipoleMatrix));
+    sElement = zeros(length(I),1); % Initialize array to hold distances
+
+    % Calculate distance using d(x,y) = sqrt((x(1)-y(1))^2+(x(2)-y(2))^2)
+     for k=1:length(I)
+         sElement(k) = sqrt((centroids(I(k),1)-centroids(J(k),1))^2+...
+             (centroids(I(k),2)-centroids(J(k),2))^2);
+     end
+
+     nearFieldDistances = sparse(I,J,sElement,N,N);
+else
     error('Sorry, generateNearFieldDistances only works when M=1.')
 end
 
-% Find locations where 
-[I,J] = ind2sub([N,N],find(multipoleMatrices));
-sElement = zeros(length(I),1);
- 
- for k=1:length(I)
-     sElement(k) = sqrt((centroids(I(k),1)-centroids(J(k),1))^2+...
-         (centroids(I(k),2)-centroids(J(k),2))^2);
- end
- 
- nearFieldDistances = sparse(I,J,sElement,N,N);
- 
 end
---
+++ modules/galerkinSolver/assembleNearFieldMatrices.m
@@ -0,0 +1,19 @@
+% assembleNearFieldMatrices.m
+% Created: 02-01-2017 by JDR at UD
+% Last Modified: 
+%
+% Input:
+%
+% Output:
+%
+% Outputs the near field components of the AIM algorithm for computing the
+% action of the time-harmonic Lippmann-Schwinger equation on a vector. Uses
+% p=0 piecewise constant elements on each element and approximates
+% integrals on triangles with an exact formula on an equal area circle. 
+
+function nearFieldMatrix = assembleNearFieldMatrices(triAreas,nearFieldDistances,c,c0,s,N)
+
+
+
+
+end
